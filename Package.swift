ba05ab0c1f4a9e12d63017100064ae93a8889d0c
zsy78192 <zhangchao@itoytoy.com>
2025-07-25 10:01:53 +0800

    fix: 调整package

--- Package.swift
+++ Package.swift
@@ -1,3 +1,7 @@
+// swift-tools-version:6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
 
 let package = Package(
     name: "DiceParser",
